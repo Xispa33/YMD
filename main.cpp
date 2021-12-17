8c487d7657e36f00ce2efc2602261d56d1e054f4
LaXispa33 <brice.leal@outlook.fr>
2021-12-17 10:29:49 +0100

    Updating README and fixing bug

--- main.cpp
+++ main.cpp
@@ -3,8 +3,11 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-
+    //qDebug() << "hello";
+    //return app.exec();
     FenetrePrincipale appli;
     appli.show();
+
     return app.exec();
+
 }
